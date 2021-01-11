3419d71edb1dc937b132c325a9410004813510f2
NahrulK <khayattullah73@gmail.com>
2021-01-11 16:15:46 +0800

    first commit

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    // Private Player Animation player_Animation
+
+    private Rigidbody myBody;
+
+    public float walk_Speed = 3f;
+    public float z_Speed = 1.5f;
+
+    private float rotation_Y = -90f;
+    private float rotation_Speed = -15f;
+    
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+        // player_Animation = GetComponentInChildren<PlayerAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePlayer();
+    }
+
+    void FixedUpdate() {
+        DetectMovement();
+    }
+
+    void DetectMovement(){
+        myBody.velocity = new Vector3(
+            Input.GetAxisRaw(Axis.HORIZONTAL_AXIS)* (-walk_Speed),
+            myBody.velocity.y,
+            Input.GetAxisRaw(Axis.VERTICAL_AXIS) * (-z_Speed)
+        );
+    } // Movemennt
+
+    void RotatePlayer() {
+        if(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) > 0) {
+            transform.rotation = Quaternion.Euler(0f, rotation_Y, 0f);
+        } else if (Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) < 0) {
+            transform.rotation = Quaternion.Euler(0f,Mathf.Abs(rotation_Y), 0f);
+        }
+    }
+
+
+} // Class
